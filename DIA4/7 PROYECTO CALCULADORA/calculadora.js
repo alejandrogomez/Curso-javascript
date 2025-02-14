3e2d72334904ee16eafb61612d54fbfdd4aaaebc
Luis Gomez Arreaga <gomezarreaga@gmail.com>
2025-02-14 14:26:53 -0500

    commit 14 febrero

---
+++ DIA4/7 PROYECTO CALCULADORA/calculadora.js
@@ -0,0 +1,93 @@
+function mostrarresultado(resultado){
+    document.getElementById("resultado").value = resultado;
+
+}
+
+function suma(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarresultado(numero1+numero2)
+
+}
+
+function resta(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarresultado(numero1-numero2)
+
+}
+
+
+function multiplicacion(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarresultado(numero1*numero2)
+
+}
+
+function division(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarresultado(numero1/numero2)
+
+}
+
+
+function raiz(){
+    let numero = +document.getElementById("campo2").value;
+    
+    mostrarresultado(Math.sqrt(numero));
+
+}
+
+
+function potencia(){
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+    mostrarresultado(Math.pow(numero1,numero2));
+
+
+}
+
+
+function absoluto(){
+    let numero = +document.getElementById("campo2").value;
+    
+    mostrarresultado(Math.abs(numero));
+
+}
+
+
+function aleatorio(){
+    let minimo = +document.getElementById("campo1").value;
+    let maximo = +document.getElementById("campo2").value;
+    maximo=maximo+1;
+    mostrarresultado(Math.floor(Math.random()* (maximo-minimo)+minimo));
+
+
+}
+
+
+
+
+function redondeo(){
+    let resultado = +document.getElementById("resultado").value;
+    
+    mostrarresultado(Math.round(resultado));
+
+}
+
+
+function techo(){
+    let resultado = +document.getElementById("resultado").value;
+    
+    mostrarresultado(Math.ceil(resultado));
+
+}
+
+function piso(){
+    let resultado = +document.getElementById("resultado").value;
+    
+    mostrarresultado(Math.floor(resultado));
+
+}
